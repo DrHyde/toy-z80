451d5433ce2a642601f34af7967a2ee821c3e0d5
David Cantrell <david@cantrell.org.uk>
2023-03-15 19:55:11 +0000

    Boot and start clock, built using z80masm from CPU::Z80::Assembler

--- machine.pl
+++ machine.pl
@@ -7,6 +7,7 @@ use Time::HiRes qw(setitimer ITIMER_VIRTUAL ITIMER_REAL);
 use Term::ReadKey;
 use IO::Scalar;
 use Data::Dumper;
+use Curses;
 
 my $cpu = CPU::Emulator::Z80->new( # RAM is 64K of zeroes
     ports => 256,
@@ -48,22 +49,78 @@ $cpu->add_output_device(address => 0x01, function => \&mem_unbank);
 $cpu->add_output_device(address => 0x02, function => \&io_wr_stdout);
 $cpu->add_output_device(address => 0xFF, function => \&hw_start_clock);
 
-setitimer(ITIMER_VIRTUAL, 1, 1);
-# setitimer(ITIMER_REAL, 1, 0.2);
-$SIG{VTALRM} = sub {
-# $SIG{ALRM} = sub {
-    my $key = ReadKey(-1);
-    if($key) {
-        # print "Got char $key\n";
+# setitimer(ITIMER_VIRTUAL, 1, 1);
+# $SIG{VTALRM} = sub {
+setitimer(ITIMER_REAL, 0.01, 0.01);
+$SIG{ALRM} = sub {
+    if(my $key = ReadKey(-1)) {
+        $cpu->{STOPREACHED} = 1;
+        warn "Got char $key\n";
     }
-    $cpu->interrupt() if($clock);
+    if($clock) {
+        $cpu->nmi();
+        # print "clock tick";
+    }
 };
 
 ReadMode 'noecho';
 ReadMode 'cbreak';
-$cpu->run(100000);
 
-print Dumper($cpu->memory());
+initscr;
+curs_set(0); # hide cursor
+my $win = Curses->new;
+
+# y, x; origin is 1,1 top left
+$win->addstr(1, 1,  "Registers");
+$win->addstr(6, 15, "SZ ½ PAC");
+$win->addstr(7, 15, "ie c ada");
+$win->addstr(8, 15, "gr a rdr");
+$win->addstr(9, 15, "no r //r");
+$win->addstr(10, 15, "   r OSy");
+$win->addstr(11, 15, "   y vu ");
+$win->addstr(5, 1,  "A:");
+$win->addstr(5, 10, "F:");
+$win->addstr(6, 1,  "BC:");
+$win->addstr(7, 1,  "DE:");
+$win->addstr(8, 1,  "HL:");
+
+$win->addstr(3, 1, "PC:");
+$win->addstr(3, 24, "Stack  SP:");
+$win->addstr(5, 42, "(top)");
+
+$win->addstr(16, 24, "Clock:");
+
+while(!$cpu->stopped()) {
+    $cpu->run(1000);
+    $win->addstr(3,  5, sprintf("0x%04x", $cpu->register('PC')->get()));
+    $win->addstr(5,  5, sprintf("0x%02x", $cpu->register('A')->get()));
+    $win->addstr(5, 13, sprintf("0x%08b", $cpu->register('F')->get()));
+    $win->addstr(6,  5, sprintf("0x%04x", $cpu->register('BC')->get()));
+    $win->addstr(7,  5, sprintf("0x%04x", $cpu->register('DE')->get()));
+    $win->addstr(8,  5, sprintf("0x%04x", $cpu->register('HL')->get()));
+
+    my $sp = $cpu->register('SP')->get();
+    $win->addstr(3, 35, sprintf("0x%04x", $sp));
+
+    # foreach my $depth (0 .. 9) {
+    #     $win->addstr(5 + $depth, 28, sprintf("0x%04x", $sp - $depth * 2));
+    #     $win->addstr(5 + $depth, 35, sprintf("0x%04x", $cpu->memory->peek16($sp - $depth * 2)));
+    #     $sp = 0x10000 if($sp == 0);
+    #  }
+
+    $win->addstr(16, 31, sprintf(
+        "0x%04x:%04x",
+        $cpu->memory->peek16(0x0064),
+        $cpu->memory->peek16(0x0062)
+    ));
+
+    $win->refresh();
+    select(undef, undef, undef, 0.05);
+}
+
+do_reset();
+
+# print Dumper($cpu->memory());
 print $cpu->format_registers();
 
 sub mem_bank {
@@ -81,7 +138,14 @@ sub hw_start_clock {
     $clock = 1;
 }
 
+my $have_done_reset = 0;
 END {
-    # ReadMode 'normal';
+    do_reset() unless($have_done_reset);
+}
+
+sub do_reset {
+    $have_done_reset++;
+    endwin;
     ReadMode 'restore';
+    # curs_set(1);
 }
